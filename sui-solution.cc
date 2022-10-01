f1fe6c4f0640c173e13b1db18c39a67ac342ed4a
Tomas Krivanek <tkrivanek2@gmail.com>
2022-10-01 23:23:39 +0200

    [ADD] finished BFS and DFS

--- sui-solution.cc
+++ sui-solution.cc
@@ -1,104 +1,128 @@
 #include "search-strategies.h"
-#include <deque>
 #include <queue>
+#include <stack>
 #include <set>
+#include "memusage.h"
 
+typedef struct {
+	std::shared_ptr<SearchState> parent;
+	SearchAction parent_act;
+	int depth; // Just for DFS, no need in BFS
+} Node;
 std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
-	std::map<const SearchState, std::tuple<const SearchState, SearchAction>>  closed;
-	std::deque<std::shared_ptr<SearchState>> working_tree;
-	std::shared_ptr<SearchState> final_state = std::make_shared<SearchState>(init_state);
-	working_tree.push_back(final_state);
-	bool found_result = false;
-	while(!working_tree.empty()){
-		SearchState working_state(*working_tree.front());
-		working_tree.pop_front();
-		auto actions = working_state.actions();
-		for(size_t i = 0; i < actions.size(); i++){
-			auto new_state = actions[i].execute(working_state);
-			std::tuple<const SearchState, SearchAction> new_tuple = {working_state,actions[i]};
-			auto check = closed.find(new_state);
-			if(check == closed.end()){
-				closed.insert({new_state,new_tuple});
-				working_tree.push_back(std::make_shared<SearchState>(new_state));
-			}
-			if(new_state.isFinal()){
-				found_result = true;
-				SearchState working_state(new_state);
-				final_state = std::make_shared<SearchState>(working_state);
-				break;
-			}
-		}
-		if(found_result)
-			break;
-	}
-	
-	std::vector<SearchAction> solution;
-	if(found_result){
-		std::shared_ptr<const SearchState> cur_state = final_state;
-		while(true){
-			auto search = closed.find(*cur_state);
-			if(search == closed.end()){
-				break;
-			}
-			auto tpl = search->second;
-			auto old_state = cur_state;
-			cur_state = std::make_shared<const SearchState>(std::get<0>(tpl));
-			auto new_action = SearchAction (std::get<1>(search->second));
-			solution.push_back(new_action);
-			closed.erase(*old_state);
-		}
-		return std::vector<SearchAction> (solution.rbegin(), solution.rend());
-	}
+	std::set<SearchState> closed;
+	std::queue<std::shared_ptr<SearchState>> open;
+	std::map<std::shared_ptr<SearchState>, Node> tree;
 
+	if(init_state.isFinal()){
+		return {};
+	}
+	bool reached_final = false;
+	std::shared_ptr<SearchState> parent_state = std::make_shared<SearchState>(init_state);
+	open.push(parent_state);
+
+	auto old_memory = getCurrentRSS(); 
+	while(!open.empty()){
+		auto current_parent = open.front();
+		SearchState working_state(*current_parent);
+		open.pop();
+		auto actions = working_state.actions();
+		auto taken_memory = getCurrentRSS();
+		if((taken_memory - old_memory)*4 + taken_memory > mem_limit_){
+			return {};
+		}
+		old_memory = taken_memory;
+		for(auto act : actions){
+			auto new_state = act.execute(working_state);
+			if(closed.count(new_state) == 0){
+				closed.insert(new_state);
+				auto new_shared = std::make_shared<SearchState>(new_state);
+				open.push(new_shared);
+				Node parent_node = {current_parent, act, 0};		
+				tree.insert({new_shared,parent_node});
+				if(new_state.isFinal()){
+					reached_final = true;
+					parent_state = new_shared;
+					break;
+				}
+			}
+		}
+		if(reached_final){
+			break;
+		}
+	}
+	if(reached_final){
+		std::vector<SearchAction> solution;
+		while(true){
+			auto tree_find = tree.find(parent_state);
+			if(tree_find == tree.end()){
+				break;
+			}
+			auto node = tree_find->second;
+			solution.insert(solution.begin(),node.parent_act);
+			parent_state = node.parent;
+		}
+		return solution;
+	}
 	return {};
 }
 
 std::vector<SearchAction> DepthFirstSearch::solve(const SearchState &init_state) {
-	std::map<const SearchState, std::tuple<const SearchState, SearchAction>>  closed;
-	std::deque<std::shared_ptr<SearchState>> working_tree;
-	std::shared_ptr<SearchState> final_state = std::make_shared<SearchState>(init_state);
-	working_tree.push_back(final_state);
-	bool found_result = false;
-	while(!working_tree.empty()){
-		SearchState working_state(*working_tree.front());
-		working_tree.pop_front();
-		
-		auto actions = working_state.actions();
-		for(size_t i = actions.size()-1; i >= 0 ; i--){
-			auto new_state = actions[i].execute(working_state);
-			std::tuple<const SearchState, SearchAction> new_tuple = {working_state,actions[i]};
-			auto check = closed.find(new_state);
-			if(check == closed.end()){
-				closed.insert({new_state,new_tuple});
-				working_tree.push_front(std::make_shared<SearchState>(new_state));
-			}
-			if(new_state.isFinal()){
-				found_result = true;
-				SearchState working_state(new_state);
-				final_state = std::make_shared<SearchState>(working_state);
-				break;
-			}
-		}
-		if(found_result)
-			break;
+	std::set<SearchState> closed;
+	std::stack<std::shared_ptr<SearchState>> open;
+	std::map<std::shared_ptr<SearchState>, Node> tree;
+	if(init_state.isFinal()){
+		return {};
 	}
-	
-	std::vector<SearchAction> solution;
-	if(found_result){
-		std::shared_ptr<const SearchState> cur_state = final_state;
+
+	bool reached_final = false;
+	std::shared_ptr<SearchState> parent_state = std::make_shared<SearchState>(init_state);
+	int current_depth = 0;
+	Node init_node = {parent_state, init_state.actions()[0], 0};
+	open.push(parent_state);
+	tree.insert({parent_state,init_node});
+
+	while(!open.empty()){
+		auto current_parent = open.top();
+		SearchState working_state(*current_parent);
+		current_depth = tree.find(current_parent)->second.depth;
+		open.pop();
+		if(current_depth >= depth_limit_){
+			continue;
+		}
+		auto actions = working_state.actions();
+		for(auto act : actions){
+			auto new_state = act.execute(working_state);
+			if(closed.count(new_state) == 0){
+				closed.insert(new_state);
+				auto new_shared = std::make_shared<SearchState>(new_state);
+				open.push(new_shared);
+				Node parent_node = {current_parent, act, current_depth+1};		
+				tree.insert({new_shared,parent_node});
+				if(new_state.isFinal()){
+					reached_final = true;
+					parent_state = new_shared;
+					break;
+				}
+			}
+		}
+		if(reached_final){
+			break;
+		}
+	}
+
+	if(reached_final){
+		std::vector<SearchAction> solution;
 		while(true){
-			auto search = closed.find(*cur_state);
-			if(search == closed.end()){
+			auto tree_find = tree.find(parent_state);
+			if(tree_find->second.depth == 0){
 				break;
 			}
-			auto tpl = search->second;
-			auto old_state = cur_state;
-			cur_state = std::make_shared<const SearchState>(std::get<0>(tpl));
-			auto new_action = SearchAction (std::get<1>(search->second));
-			solution.push_back(new_action);
-			closed.erase(*old_state);
+			auto node = tree_find->second;
+			solution.insert(solution.begin(),node.parent_act);
+			parent_state = node.parent;
 		}
-		return std::vector<SearchAction> (solution.rbegin(), solution.rend());
+		return solution;
 	}
 
 	return {};
